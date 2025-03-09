e8ef5b1ceede63d50091665771af4b2d62b97a9d
whansen340 <peteyhansen340@gmail.com>
2025-03-08 19:08:00 -0700

    description

--- recipes.mjs
+++ recipes.mjs
@@ -4,21 +4,21 @@ export const recipes = [
         name: "Chicken Curry",
         image: "images/chicken-curry.webp",
         rating: 4,
-        description: "A classic Italian pasta dish with rich meat sauce."
+        description: "One of Indias top dishesand always a good choice."
     },
     {
         id: 2,
         name: "Apple Crisp",
         image: "images/apple-crisp.jpg",
         rating: 5,
-        description: "A moist and delicious chocolate cake with creamy frosting."
+        description: "A perfect sweet and tard dessert."
     },
     {
         id: 3,
         name: "Roasted Potatoes",
         image: "images/roasted-potatoes.webp",
         rating: 3,
-        description: "A fresh and crispy salad with homemade Caesar dressing."
+        description: "Who doesn't like a good potatoe dish!."
     }
 ];
 
