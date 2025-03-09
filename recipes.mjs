86e86bfc9d27e1ffd6beed10190f6b5fb7fa1813
whansen340 <peteyhansen340@gmail.com>
2025-03-08 17:25:17 -0700

    pictures for recipes

--- recipes.mjs
+++ recipes.mjs
@@ -1,22 +1,22 @@
 export const recipes = [
     {
         id: 1,
-        name: "Spaghetti Bolognese",
-        image: "images/spaghetti.jpg",
+        name: "Chicken Curry",
+        image: "images/chicken-curry.webp",
         rating: 4,
         description: "A classic Italian pasta dish with rich meat sauce."
     },
     {
         id: 2,
-        name: "Chocolate Cake",
-        image: "images/chocolate-cake.jpg",
+        name: "Apple Crisp",
+        image: "images/apple-crisp.jpg",
         rating: 5,
         description: "A moist and delicious chocolate cake with creamy frosting."
     },
     {
         id: 3,
-        name: "Caesar Salad",
-        image: "images/caesar-salad.jpg",
+        name: "Roasted Potatoes",
+        image: "images/roasted-potatoes.webp",
         rating: 3,
         description: "A fresh and crispy salad with homemade Caesar dressing."
     }
